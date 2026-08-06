14d958f78a0d1b911ef74d270921a19d8d3b2415
Truong Anh Vu <truonganhvu205@gmail.com>
2026-08-06 17:44:06 +0700

    first commit

--- embeddings.py
+++ embeddings.py
@@ -2,8 +2,9 @@ from chunking import chunking
 from FlagEmbedding import BGEM3FlagModel
 
 def embeddings():
+    docs = chunking()
     model = BGEM3FlagModel('BAAI/bge-m3',  use_fp16=True)
-    sentences = [doc.page_content for doc in chunking()]
+    sentences = [doc.page_content for doc in docs]
 
     return model.encode(sentences,
                         batch_size=12,
